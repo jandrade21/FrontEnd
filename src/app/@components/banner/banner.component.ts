7207960166541a2fb21b20a4767b67666a1c8197
jonatan <yoni.lp91@gmail.com>
2022-07-26 00:02:54 -0300

    modificar datos de persona

--- src/app/@components/banner/banner.component.ts
+++ src/app/@components/banner/banner.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/service/login.service';
 import { Router } from '@angular/router';
 import { ModalService } from 'src/app/service/modal.service';
+import { Persona } from 'src/app/class/persona';
+
 
 @Component({
   selector: 'app-banner',
@@ -10,14 +12,24 @@ import { ModalService } from 'src/app/service/modal.service';
 })
 export class BannerComponent implements OnInit {
 
+  persona: Persona = new Persona;
+  datatable:any=[];
   ulogged: string =""
-  constructor(private LoginService:LoginService, private router:Router,private modalservice:ModalService) { }
+  constructor(private LoginService:LoginService, private router:Router,
+    private modalservice:ModalService) {
+  
+     }
 
   ngOnInit(): void {
     this.ulogged = this.LoginService.getUserLogged();
-
-
+    this.onDateTable();
   }
 
-
+  onDateTable(){
+    this.modalservice.getPersona().subscribe(res=>{
+      this.datatable=res;
+      console.log(res);
+    });
+  }
+      
 }
